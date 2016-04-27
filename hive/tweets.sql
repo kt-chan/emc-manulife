bbecee2bfabeaecf9fa850b06a7bab2effd2032b
dwong-pivotal <derrick.chwong@gmail.com>
2016-04-27 22:40:51 +0800

    add twitter manulife stream

--- hive/tweets.sql
+++ hive/tweets.sql
@@ -31,3 +31,35 @@ ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 STORED AS TEXTFILE;
 
 load data inpath '/xd/tweets_hdfs/*' overwrite into table tweets;
+
+
+CREATE TABLE tweets_manulife (
+  id BIGINT,
+  created_at STRING,
+  source STRING,
+  favorited BOOLEAN,
+retweet_count INT,
+  retweeted_status STRUCT<
+    text:STRING,
+    user:STRUCT<screen_name:STRING,name:STRING>,
+    retweet_count:INT>,
+  entities STRUCT<
+    urls:ARRAY<STRUCT<expanded_url:STRING>>,
+    user_mentions:ARRAY<STRUCT<screen_name:STRING,name:STRING>>,
+    hashtags:ARRAY<STRUCT<text:STRING>>>,
+  text STRING,
+  user STRUCT<
+    screen_name:STRING,
+    name:STRING,
+    friends_count:INT,
+    followers_count:INT,
+    statuses_count:INT,
+    verified:BOOLEAN,
+    utc_offset:INT,
+    time_zone:STRING>,
+  in_reply_to_screen_name STRING
+) 
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+STORED AS TEXTFILE;
+
+load data inpath '/xd/tweets_manulife/*' overwrite into table tweets_manulife;
